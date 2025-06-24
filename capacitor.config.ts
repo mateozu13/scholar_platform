f5dc40e8ce7ca2a0090997bf76b20bf0c1d6af3c
Mateo Zurita <mateozurita100@gmail.com>
2025-06-24 17:16:42 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyecto_final_movil',
+  webDir: 'www'
+};
+
+export default config;
